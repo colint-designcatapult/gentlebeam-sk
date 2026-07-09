731dbfc8224fded1993a65a5406a4c39d1199120
brendanh-dc <brendanh@designcatapult.com>
2026-07-09 16:54:16 -0700

    bug/gbsk-9-hvps-internal-adcs-report-invalid-data
    [GBSK-9] HVPS internal ADCs report invalid data https://op.designcatapult.com/wp/GBSK-9

--- hvps-interface/main/Sensus Src/adc.c
+++ hvps-interface/main/Sensus Src/adc.c
@@ -6,23 +6,16 @@
 #include "monitoring.h"
 #include "timers.h"
 
-uint32_t adc_int[NUM_INT_ADC];
+uint16_t adc_int[NUM_INT_ADC];
 uint32_t adc_int_sums[NUM_INT_ADC];
 uint32_t adc_int_buf[NUM_INT_ADC][16];	//TBD TODO magic number
 
 void setup_adc()
 {
-	int i = 0;
 	hadc1.Instance->CR2 |= ADC_CR2_CAL;
-	/*do
-	{
-		i++;
-	} while(hadc1.Instance->CR2 & ADC_CR2_CAL);*/
-	for(i = 0; i < 100000;)
-	{
-		i++;
-	}
-	HAL_ADC_Start_DMA(&hadc1, adc_int, NUM_INT_ADC);
+
+	HAL_Delay(10);
+	HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_int, NUM_INT_ADC);
 	int_adc_ms = 5;	//TBD TODO placeholder/magic number
 }
 
@@ -38,7 +31,7 @@ void process_adc()
 		report_int_adc_vals(adc_int);
 
 		//Restart ADC DMA
-		HAL_ADC_Start_DMA(&hadc1, adc_int, NUM_INT_ADC);
+		HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_int, NUM_INT_ADC);
 		int_adc_ms = 5;	//TBD TODO placeholder/magic number
 	}
 }
